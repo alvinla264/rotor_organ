36be33af65d25bff156a25d54448c4f4a1651813
Alvin La <laa5@uci.edu>
2023-11-12 18:19:31 -0800

    removed lcd code and fixed motor test code

--- rotor_organ.ino
+++ rotor_organ.ino
@@ -1,22 +1,23 @@
 #include <Servo.h>
 #include "motor_control.h"
 #include "lcd_screen.h"
-#define ESC_PIN 5
-#define POT A1
+#define ESC_PIN 9
+#define POT A0
 Servo ESC;
-LCDScreen lcd;
+//LCDScreen lcd;
 void setup(){
-    ESC.attach(ESC_PIN);
+    ESC.attach(ESC_PIN, 1000, 2000);
     Serial.begin(9600);
 }
 
 void loop(){
     int potValue = analogRead(POT);
     potValue =  map(potValue, 0, 1023, 1000, 2000);
+    Serial.println(potValue);
     ESC.writeMicroseconds(potValue);
-    int freq = map(potValue, 1000, 2000, 0, 50000);
-    char second_line[17];
-    sprintf(second_line, "%d", freq);
-    lcd.PrintBothLine("Frequency", second_line);
-    
+    //int freq = map(potValue, 1000, 2000, 0, 50000);
+    //char second_line[17];
+    //sprintf(second_line, "%d", freq);
+    //lcd.PrintBothLine("Frequency", second_line);
+    delay(150);
 }
