a3fe1473c6827cb73acf4b71ebd5a7cbfa24bf45
Alvin <laa5@uci.edu>
2024-04-14 22:23:37 -0700

    separated music functions into own file

--- motor_control.h
+++ motor_control.h
@@ -2,25 +2,13 @@
 #define MOTOR_CONTROL_H_
 #include <Arduino.h>
 #include <Servo.h>
+#include "music.h"
 
 //max freq is G# 2 octaves up 1661.2(6 octave)
 #define num_of_plates 8
 #define MAX_RPM 9480
 #define MIN_RPM 500
-enum Note{
-  C = 0,
-  C_S,
-  D,
-  D_S,
-  E,
-  F,
-  F_S,
-  G,
-  G_S,
-  A,
-  A_S,
-  B
-};
+
 class MotorControl{
     private:
         int ESC_pin;
@@ -39,8 +27,5 @@ class MotorControl{
         int GetMotorOutput(){return motor_output;}
         void writeMicroseconds(int value);
 };
-//fourth octave
-Note strToEnum(String note);
-float getFrequency(String note);
-String enumToString(Note n);
+
 #endif //MOTOR_CONTROL_H_
---
+++ music.h
@@ -0,0 +1,67 @@
+#ifndef MUSIC_H_
+#define MUSIC_H_
+
+enum Note{
+  C = 0,
+  C_S,
+  D,
+  D_S,
+  E,
+  F,
+  F_S,
+  G,
+  G_S,
+  A,
+  A_S,
+  B
+};
+
+
+Note strToEnum(String note){
+  if(note == "C") return C;
+  else if(note == "C#") return C_S;
+  else if(note == "D") return D;
+  else if(note == "D#") return D_S;
+  else if(note == "E") return E;
+  else if(note == "F") return F;
+  else if(note == "F#") return F_S;
+  else if(note == "G") return G;
+  else if(note == "G#") return G_S;
+  else if(note == "A") return A;
+  else if(note == "A#") return A_S;
+  else if(note == "B") return B;
+  else return C;
+}
+
+String enumToString(Note n){
+  switch(n){
+    case C:
+      return "C";
+    case C_S:
+      return "C#";
+    case D:
+      return "D";
+    case D_S:
+      return "D#";
+    case E:
+      return "E";
+    case F:
+      return "F";
+    case F_S:
+      return "F#";
+    case G:
+      return "G";
+    case G_S:
+      return "G#";
+    case A:
+      return "A";
+    case A_S:
+      return "A#";
+    case B:
+      return "B";
+    default:
+      return "C";
+  }
+}
+
+#endif //MUSIC_H_
